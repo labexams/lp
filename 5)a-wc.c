331074370e30454df0626c9458f01274ec1da1ec
labexams <86876820+labexams@users.noreply.github.com>
2022-11-24 21:14:23 +0530

    Update 5)a-wc.c

--- 5)a-wc.c
+++ 5)a-wc.c
@@ -8,30 +8,16 @@ void main(int argc,char *argv[]){
         while((ch=getc(fptr))!=EOF){
                 if(ch==' '){
                         while(((ch=getc(fptr))!=EOF) || ((ch=getc(fptr))!=' ')){
-                            c++;
-                            break;
+                                c++;
+                                break;
                         }
                         w++;
                         ungetc(ch,stdin);
                 }
                 if(ch=='\n'){
-                    w++,l++;
+                        w++,l++;
                 }
                 c++;
         }
-        if(argc==1)p{
-            printf("\nLines are: %d\nCharacters are: %d\nWords are: %d\n",l,c,w);
-        }else{
-                for(int i=2;i<=argc;i++){
-                        ch=argc[i]
-                        switch(ch){
-                                case "-l":printf("\nLines are %d:",l);
-                                        break;
-                                case "-w":printf("\nWords are %d:",w);
-                                        break;
-                                case "-c":printf("\nCharacters are %d",c);
-                                default :printf("\nInvalid argument given");
-                        }
-                }
-        }
+        printf("\nCharacters are %d\nWords are %d\nLines are %d",c,w,l);
 }
