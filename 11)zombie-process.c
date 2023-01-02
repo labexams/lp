ecafc9df1486740e0891d0b0f328ac875149e468
labexams <86876820+labexams@users.noreply.github.com>
2023-01-02 08:02:27 +0530

    Update 11)zombie-process.c

--- 11)zombie-process.c
+++ 11)zombie-process.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-int main(){
-
-    int pid = fork();
-    if (pid ==  0){
-        printf("child process %d\n",getpid());
-	exit(0);
-     }
-    else {
-        wait(0);
-	sleep(10);
-    	printf("Parent process\n");
-       }
-    return 0;
+int main() {
+  int pid = fork();
+  if (pid == 0) {
+    printf("Child process %d\n", getpid());
+    exit(0);
+  } else {
+    wait(0);
+    sleep(10);
+    printf("Parent process\n");
+  }
+  return 0;
 }
